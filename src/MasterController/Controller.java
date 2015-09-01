540b6a5d8c58a270e6462b274b694f9b07bb7948
jkt001 <jk_john_kevin@hotmail.com>
2015-09-01 14:11:36 +0800

    relegate input and output process to respective classes in the logic package and modified input to make all ignored words stored in lower case (and trimmed)

--- src/MasterController/Controller.java
+++ src/MasterController/Controller.java
@@ -3,7 +3,6 @@ package MasterController;
 import logic.TitleInput;
 
 import java.util.Scanner;
-import java.util.ArrayList;
 
 import logic.Shifter;
 import logic.Sorter;
@@ -26,49 +25,10 @@ public class Controller {
 	}
 
 	public void run() {
-		System.out.print("How many ignored words? ");
-		int numOfIgnoredWords = readInteger();
-
-		for (int i = 0; i < numOfIgnoredWords; i++) {
-			System.out.print("Enter Ignored Word #" + (i+1) + " = ");
-			String word = sc.nextLine();
-			addIgnoredWord(word);
-		}
-
-		System.out.print("How many titles? ");
-		int numOfTitles = readInteger();
-
-		for (int i = 0; i < numOfTitles; i++) {
-			System.out.print("Enter Title #" + (i+1) + " = ");
-			String title = sc.nextLine();
-			addTitle(title);
-		}
-
-		System.out.println("===========================================");
-		System.out.println("Result: ");
-
+		input.processInput();
 		shifter.shiftAndStore();
 		sorter.sort();
-
-		ArrayList<String> sorted = output.getSortedPermutations();
-		for (String s : sorted) {
-			System.out.println(s);
-		}
-
-	}
-
-	private int readInteger() {
-		int num = sc.nextInt();
-		sc.nextLine();
-		return num;
-	}
-
-	private void addIgnoredWord(String word) {
-		input.addIgnoredWord(word);
-	}
-
-	private void addTitle(String title) {
-		input.addTitle(title);
+		output.processOutput();
 	}
 
 }
--- src/logic/Output.java
+++ src/logic/Output.java
@@ -4,17 +4,34 @@ import model.DataStorage;
 import java.util.ArrayList;
 
 public class Output {
-	
+
 	private DataStorage activeStorage;
 	private ArrayList<String> sortedPermutations;
-	
+	private final String DIVIDER = "======================================================";
+
 	public Output() {
 		this.activeStorage = DataStorage.getInstance();
-		
 	}
-	
-	public ArrayList<String> getSortedPermutations() {
+
+	private void setSortedPermutations() {
 		this.sortedPermutations = activeStorage.getSortedPermutations();
-		return sortedPermutations;
 	}
+
+	public void processOutput() {
+		this.setSortedPermutations();
+
+		System.out.println("\nResult: ");
+		System.out.println(DIVIDER);
+
+		printAllPermutations();
+
+		System.out.println(DIVIDER);
+	}
+
+	private void printAllPermutations() {
+		for (int i = 1; i <= sortedPermutations.size(); i++) {
+			System.out.println("Permutation #" + i + " : " + sortedPermutations.get(i - 1));
+		}
+	}
+
 }
